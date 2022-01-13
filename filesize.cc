b1f45a27b6725ebfe2468dc3ed21db412d6b8022
Justin Garey <jg390518@ohio.edu>
2022-01-13 00:10:07 -0500

    The program works as expected

--- filesize.cc
+++ filesize.cc
@@ -7,12 +7,31 @@ using namespace std;
 //program: ./filesize
 // input.3lines: 3
 // input.1line: 1
+//
+
+/**
+  * getLines: Returns the number of lines in a file
+  * @param filename : The name of the file to access
+  * @return : Returns the an integer of the number of lines in the file
+ **/
+int getLines(string filename){
+    ifstream ins;                          // Create variable to hold file
+    ins.open(filename);                    // Open file
+    if (ins.fail()) return -1;             // If file cannot be opened return -1
+    string line;                           // Create variable to hold the line
+    int count = 0;                         // Create counter variable
+    while (getline(ins, line)) count++;    // If we have a line, add to count
+    return count;                          // Return the count
+}
+
 
 int main( int argc, char* argv[] )
 {
-    // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 0; arg < argc; ++arg)
-            std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
+    // Print out program
+    cout << "program: " << argv[0] << endl;
+    // Print out lines in each file
+    for (int arg = 1; arg < argc; ++arg)
+            cout << " " << argv[arg] << ": " << getLines(argv[arg]) << endl;
 
     exit(0); // this means that the program executed correctly!
 }
